4cf4a1ef4a3a53d7d59b7f75473bb76e92534502
Mohamed-Hatem153 <mohamedhatemakl@gmail.com>
2021-11-11 15:36:51 +0200

    complete why choose glila section without responsive

--- src/components/WhyChooseUs/WhyChooseUs.js
+++ src/components/WhyChooseUs/WhyChooseUs.js
@@ -1,6 +1,9 @@
 import React from "react";
 import leaf from "../../Assets/images/leaf.png";
 import { IoMdTrendingUp } from "react-icons/io";
+import { MdLock } from "react-icons/md";
+import { AiOutlineTeam } from "react-icons/ai";
+import { GiDiploma } from "react-icons/gi";
 import "./WhyChooseUs.css";
 
 const WhyChooseUs = () => {
@@ -8,17 +11,22 @@ const WhyChooseUs = () => {
     <div className="whychooseus">
       <div className="container">
         <div className="whychooseus-title">
-          <p>we are gelila</p>
-          <h3>why choose gelila.?</h3>
-          <div>
-            <img src={leaf} alt="leaf" />
+          <div className="grid">
+            <div>
+              <p>we are gelila</p>
+              <h3>why choose gelila.?</h3>
+              <div>
+                <img src={leaf} alt="leaf" />
+              </div>
+            </div>
           </div>
         </div>
         <div className="whygelila">
           <div className="whyus">
             <p>
-              Gelila always ensures the maximum quality and we already consider
-              international certification that support our consumers trust.
+              <span>GELILA</span> always ensures the maximum quality and we
+              already consider international certification that support our
+              consumers trust.
             </p>
           </div>
           <div className="whyreasons grid">
@@ -26,48 +34,44 @@ const WhyChooseUs = () => {
               <span className="icon">
                 <IoMdTrendingUp />
               </span>
-              <h6>Modern Design</h6>
+              <h6>Highest Quality Level</h6>
               <p>
-                We use latest technology for the latest world because we know
-                the demand of peoples.
+                we use the latest technology in sorting, sizing and packing,
+                From picking out the fruits and vegetables to storing and
+                delivering them we always ensure the best
               </p>
             </div>
 
-            <div>
-              <div className="item">
-                <span className="icon">
-                  <IoMdTrendingUp />
-                </span>
-                <h6>Modern Design</h6>
-                <p>
-                  We use latest technology for the latest world because we know
-                  the demand of peoples.
-                </p>
-              </div>
+            <div className="item">
+              <span className="icon">
+                <MdLock />
+              </span>
+              <h6>Secure and Seamless Processes</h6>
+              <p>
+                our processes are always secured and Seamless at any stage to
+                ensure least effort and highest time saving for our customers.
+              </p>
             </div>
-            <div>
-              <div className="item">
-                <span className="icon">
-                  <IoMdTrendingUp />
-                </span>
-                <h6>Modern Design</h6>
-                <p>
-                  We use latest technology for the latest world because we know
-                  the demand of peoples.
-                </p>
-              </div>
+
+            <div className="item">
+              <span className="icon">
+                <AiOutlineTeam />
+              </span>
+              <h6>Flexibility and Availability</h6>
+              <p>
+                we are always hearing from you and available for your inquiries.
+              </p>
             </div>
-            <div>
-              <div className="item">
-                <span className="icon">
-                  <IoMdTrendingUp />
-                </span>
-                <h6>Modern Design</h6>
-                <p>
-                  We use latest technology for the latest world because we know
-                  the demand of peoples.
-                </p>
-              </div>
+
+            <div className="item">
+              <span className="icon">
+                <GiDiploma />
+              </span>
+              <h6>Certificates</h6>
+              <p>
+                The ISO certificate was our first step towards getting certified
+                by international standards’ organizations.
+              </p>
             </div>
           </div>
         </div>
